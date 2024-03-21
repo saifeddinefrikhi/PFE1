43c45d6205f7223d6d324c1ef3d26d2c7481217c
saifeddinefrikhi <saifeddinefrikhi@gmail.com>
2024-03-22 00:43:15 +0100

    add

---
+++ PFE/myapp/src/app/(logsign)/Code/page.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Code.css'
+import { TbNumbers } from "react-icons/tb";
+import Link from 'next/link';
+
+
+
+
+const Code = () => {
+  return (
+    <div>
+      <div className='frikhy'>
+        <div className='Wrapper'>
+            <form action="">
+                <h1>Put Your Code Here</h1>
+                <div className="input-Box">
+                <input type="number" placeholder='Code' required />
+                <TbNumbers className='iconn'/>
+                </div>
+                <Link href='/NewPsw'><button  type='submit'>Continue</button></Link>
+            </form>  
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Code
---
+++ PFE/myapp/src/components/Heropage.js
@@ -0,0 +1,17 @@
+import style from "@/app/styles/heropage.module.css";
+import { Hero_content } from "./Hero_content";
+
+
+const Heropage = () => {
+  return (
+    <>
+    <section className={style.hero_page}>
+    
+    <Hero_content/>
+    
+    </section>
+    </>
+  )
+}
+
+export default Heropage
